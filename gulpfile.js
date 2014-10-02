df5f0d8de1d4c85d9be7212bb7ffceb79bb96442
Sergey Semyonov <slike9@rambler.ru>
2014-10-02 18:30:23 +0000

    gulp: watch, livereload added

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,22 @@ var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require ('./webpack.config.js');
 var gutil = require("gulp-util");
 var del = require('del');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', ['build-dev']);
+gulp.task('default', ['develop']);
+
+gulp.task('develop', ['build-dev', 'watch', 'serve']);
 
 gulp.task('build-dev', function() {
   gulpWebpack(webpackConfig)
     .pipe(gulp.dest('dist/assets/'));
   gulp.src(['src/index.html', 'src/favicon.ico'])
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['src/index.html', 'src/scripts/**/*', 'src/styles/**/*'], ['build-dev']);
 });
 
 gulp.task('clean', function (cb) {
@@ -37,4 +45,16 @@ gulp.task("webpack-dev-server", function(callback) {
         // keep the server alive or continue?
         // callback();
     });
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+    port: 8080
+  });
+
+  gulp.watch(['index.html', 'assets/main.js'], {cwd: 'dist'}, reload);
 });
