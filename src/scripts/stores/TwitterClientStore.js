99cb791a8d7afaa29761b68baa0250fc5ae86c04
Paul Yakshankin <yakspavel@undev.ru>
2014-10-02 13:21:36 +0400

    добавил запрос в twitter api

--- src/scripts/stores/TwitterClientStore.js
+++ src/scripts/stores/TwitterClientStore.js
@@ -3,10 +3,30 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var TwitterClientConstants = require('../constants/TwitterClientConstants');
+var OAuth = require('oauth');
+var oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/requesttoken',
+  'https://api.twitter.com/oauth/accesstoken',
+  'pFtPOLUXBXDtPHokigyXN9UAT',
+  'FlGNi9fhU2q3Ucfi5M9czBraY3QxEkPflLntLJgrBjA0ecMpon',
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+);
 var merge = require('react/lib/merge');
 
+
 function searchTwits(query) {
-  console.log('searchTwits:', query);
+    oauth.get(
+      'https://api.twitter.com/1.1/search/tweets.json?q=' + query + '&resulttype=recent',
+      '978792919-URT380OU0ofxqomr8hUPXOxgoJAMRqUODAGQEdoQ', //test user token
+      '4Gx37DpzwwVcBszGP51hUwegmuFnSsxUZ9IbuizUbbfMW', //test user secret
+      function (e, data, res){
+        if (e) console.error(e);
+        var twits = JSON.parse(data);
+        console.log(twits);
+        // TwitActions.create(twits["statuses"]);
+      });
 }
 
 var TwitterClientStore = merge(EventEmitter.prototype, {
