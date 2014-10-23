9efd24e5334252500a405af9d4927e078e030371
Sergey Semyonov <slike9@rambler.ru>
2014-10-23 19:19:37 +0000

    Refactoring: Searching has moved from storage to action

--- src/scripts/actions/TwitterClientActions.js
+++ src/scripts/actions/TwitterClientActions.js
@@ -2,12 +2,15 @@
 
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var TwitterClientConstants = require('../constants/TwitterClientConstants');
+var TwitterClient = require('../utils/TwitterClient');
 
 var TwitterClientActions = {
   searchTwits: function (query) {
-    AppDispatcher.handleViewAction({
-      actionType: TwitterClientConstants.SEARCH_TWITS,
-      query: query
+    TwitterClient.search(query).then(function (twits) {
+      AppDispatcher.handleViewAction({
+        actionType: TwitterClientConstants.SET_TWITS,
+        twits: twits
+      });
     });
   },
 };
--- src/scripts/stores/TwitterClientStore.js
+++ src/scripts/stores/TwitterClientStore.js
@@ -4,20 +4,10 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var TwitterClientConstants = require('../constants/TwitterClientConstants');
 var merge = require('react/lib/merge');
-var TwitterClient = require('../utils/TwitterClient');
 
 var CHANGE_EVENT = 'change';
 
 var _twits = [];
-
-function searchTwits(query) {
-  TwitterClient.search(query).then(setTwits);
-}
-
-function setTwits(twits) {
-  _twits = twits || [];
-  TwitterClientStore.emitChange();
-}
 
 var TwitterClientStore = merge(EventEmitter.prototype, {
   getAll: function () {
@@ -50,8 +40,8 @@ AppDispatcher.register(function(payload) {
   var action = payload.action;
 
   switch(action.actionType) {
-    case TwitterClientConstants.SEARCH_TWITS:
-      searchTwits(action.query);
+    case TwitterClientConstants.SET_TWITS:
+      _twits = action.twits || [];
       break;
 
     default:
@@ -62,7 +52,7 @@ AppDispatcher.register(function(payload) {
   // needs to trigger a UI change after every view action, so we can make the
   // code less repetitive by putting it here.  We need the default case,
   // however, to make sure this only gets called after one of the cases above.
-  //TwitterClientStore.emitChange();
+  TwitterClientStore.emitChange();
 
   return true; // No errors.  Needed by promise in Dispatcher.
 });
